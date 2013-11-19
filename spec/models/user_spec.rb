478b353acc2faca0ca3bd4256c187a913f14c8d4
zinux <zinux@zinux-0S.(none)>
2013-11-19 22:36:02 +0700

    end modelling s5

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -89,4 +89,35 @@ describe User do
 			specify {user_with_invalid_password.should be_false}
 		end
 	end
+
+	# exercise 1
+	describe "email address with mixed case" do
+		let(:mixed_case_email) {"Foo@ExaAMPle.CoM"}
+		it "it should be saved as all lower-case" do
+			@user.email = mixed_case_email
+			@user.save
+			expect(@user.reload.email).to eq mixed_case_email.downcase
+		end
+	end
+	#exercise 3
+	describe "when email format is invalid" do
+    it "should be invalid" do
+      addresses = %w[user@foo,com user_at_foo.org example.user@foo.
+                     foo@bar_baz.com foo@bar+baz.com]
+      addresses.each do |invalid_address|
+        @user.email = invalid_address
+        expect(@user).not_to be_valid
+      end
+    end
+  end
+
+  describe "when email format is valid" do
+    it "should be valid" do
+      addresses = %w[user@foo.COM A_US-ER@f.b.org frst.lst@foo.jp a+b@baz.cn]
+      addresses.each do |valid_address|
+        @user.email = valid_address
+        expect(@user).to be_valid
+      end
+    end
+  end
 end
